a9f008c70407410b3c7b4ca8586cbbd239f3878a
Arvid Häggqvist <arvid.haggqvist@gmail.com>
2013-06-27 17:09:40 +0200

    Some commenting

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-	// DB connection parameters
+	// This is where you define your database information
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'root');
